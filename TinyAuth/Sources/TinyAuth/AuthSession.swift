ab69bf9d51244b085ce88d1e68a783525a793aa0
Roy Hsu <roy.hsu@tinyworld.cc>
2019-08-21 09:50:29 +0800

    minors.

---
+++ TinyAuth/Sources/TinyAuth/AuthSession.swift
@@ -0,0 +1,50 @@
+// MARK: - AuthSession
+
+import TinyKeyValueStore
+
+/// AuthSession provides a common layer to communicate auth providers with the connected store.
+public final class AuthSession<Auth> {
+    
+    public var auth: Auth? { authField.wrappedValue }
+    
+    private let authField: Field<String, Auth>
+    
+    private var authTask: AuthTask?
+    
+    public init(authField: Field<String, Auth>) { self.authField = authField }
+    
+}
+
+extension AuthSession {
+    
+    /// To get authorized or revoke the existing auth.
+    /// - Parameter provider: The auth provider.
+    /// - Parameter completion: The result of the auth request.
+    ///
+    /// - Note: **DO NOT** start a new authorize before the previous one completes, otherwise,
+    ///  the session will crash the program.
+    public func authorize(
+        with provider: AuthProvider<Auth, Error>,
+        completion: @escaping (Result<Auth?, Error>) -> Void
+    ) {
+        
+        precondition(authTask == nil)
+        
+        authTask = provider { result in
+            
+            self.authTask = nil
+            
+            do {
+                
+                self.authField.wrappedValue = try result.get()
+                
+                completion(.success(self.auth))
+                
+            }
+            catch { completion(.failure(error)) }
+            
+        }
+        
+    }
+    
+}
--- TinyAuth/Tests/TinyAuthTests/AuthSessionTests.swift
+++ TinyAuth/Tests/TinyAuthTests/AuthSessionTests.swift
@@ -1,5 +1,6 @@
 // MARK: - AuthSessionTests
 
+import TinyKeyValueStore
 import XCTest
 
 @testable import TinyAuth
@@ -8,47 +9,53 @@ final class AuthSessionTests: XCTestCase {
    
     func testInitialize() {
         
-        let session = AuthSession<Auth>()
+        let memory: Memory<String, Auth> = [ "auth": Auth(user: .init(id: 1)) ]
 
-        XCTAssertNil(session.auth)
+        let session = AuthSession(
+            authField: Field(name: "auth", store: memory)
+        )
+
+        XCTAssertEqual(session.auth, Auth(user: .init(id: 1)))
         
     }
     
     func testAuthorize() {
-        
+
         let authorizeDidComplete = expectation(description: "Authorize did complete.")
-        
+
         let mockAuthProvider: AuthProvider<Auth, Error> = { completion in
-            
+
             DispatchQueue.global().async {
-                
+
                 completion(.success(Auth(user: .init(id: 1))))
-                
+
             }
-            
+
             return MockAuthTask()
-            
+
         }
-        
-        let session = AuthSession<Auth>()
-        
+
+        let session = AuthSession<Auth>(
+            authField: Field(name: "auth", store: Memory())
+        )
+
         session.authorize(with: mockAuthProvider) { result in
-            
+
             defer { authorizeDidComplete.fulfill() }
-            
+
             do {
-                
+
                 let auth = try result.get()
-                
+
                 XCTAssertEqual(session.auth, auth)
-            
+
                 XCTAssertEqual(auth, Auth(user: .init(id: 1)))
-                
+
             }
             catch { XCTFail("\(error)") }
-                
+
         }
-        
+
         waitForExpectations(timeout: 10.0)
         
     }
